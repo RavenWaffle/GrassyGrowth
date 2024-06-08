8c1d6b1553bebad0647fbbdf181ec9109a560349
RavenWaffle <nguyennhutung3011@gmail.com>
2024-06-08 23:00:50 +0700

    Sentry done

--- Assets/Waffle/Scripts/Entity/EntityStructureSentry.cs
+++ Assets/Waffle/Scripts/Entity/EntityStructureSentry.cs
@@ -7,26 +7,8 @@ namespace Structure
 {
     public class EntityStructureSentry : Entity
     {
-        [SerializeField] Animator animator;
-        [SerializeField] float _range;
-        [SerializeField] Entity target;
+        [SerializeField] private float _range;
+        public float Range => _range;
 
-        private void FixedUpdate()
-        {
-            Collider[] col = Physics.OverlapSphere(this.transform.position, _range);
-            foreach(Collider c in col)
-            {
-                if(c.TryGetComponent<Enemy.EntityEnemy>(out Enemy.EntityEnemy Target))
-                {
-                    target = Target;
-                    break;
-                }
-            }
-
-            if (target != null)
-            { 
-
-            }
-        }
     }
 }
---
+++ Assets/Waffle/Scripts/Structure/SentryBehaviour.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Structure
+{
+    public class SentryBehaviour : MonoBehaviour
+    {
+        EntityStructureSentry Base;
+        [SerializeField] Transform CharacterParent;
+        [SerializeField] Animator animator;
+        [SerializeField] Weapon.WeaponObject weapon;
+        Entity target;
+        void Start()
+        {
+            Base = this.GetComponent<EntityStructureSentry>();
+        }
+
+        private void FixedUpdate()
+        {
+            BehaviourHandler();
+            FindTarget();
+            if(target != null)
+            {
+                if (weapon.canAttack)
+                {
+                    Attack();
+                }
+            }
+        }
+
+        void FindTarget()
+        {
+            target = null;
+            Collider[] col = Physics.OverlapSphere(this.transform.position, Base.Range);
+            foreach (Collider c in col)
+            {
+                if (c.TryGetComponent<Enemy.EntityEnemy>(out Enemy.EntityEnemy Target))
+                {
+                    target = Target;
+                    break;
+                }
+            }
+        }
+
+        void Attack()
+        {
+            if (Vector3.Distance(this.transform.position, target.transform.position) <= Base.Range)
+            {
+                weapon.StartCoroutine(weapon.Attack(target));
+            }
+        }
+
+        void BehaviourHandler()
+        {
+            if (weapon.attacking)
+            {
+                animator.SetBool("attacking", true);
+            }
+            else
+            {
+                animator.SetBool("attacking", false);
+            }
+
+            if(target != null)
+            {
+                Debug.Log(target.name);
+                Vector3 safeguard = target.transform.position;
+                safeguard.Scale(new Vector3(0, -1, -1));
+                CharacterParent.transform.LookAt(safeguard);
+            }
+        }
+    }
+}
